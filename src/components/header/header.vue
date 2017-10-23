a8f945d997c3689f75fe911900a5728c03092d47
leiger <leiger@bupt.edu.cn>
2017-10-23 09:44:58 -0400

    add grey box using sticky footer

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,18 +17,28 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div class="support-count" v-if="seller.supports">
+      <div class="support-count" v-if="seller.supports" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" alt="" width="100%" height="100%">
+    </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -40,8 +50,18 @@
        type: Object
      }
    },
+   data() {
+     return {
+       detailShow: false
+     }
+   },
    created() {
      this.classMap = ['decrease', 'guarantee', 'special', 'invoice', 'discount']
+   },
+   methods: {
+     showDetail () {
+       this.detailShow = true
+     }
    }
  }
 </script>
@@ -53,6 +73,7 @@
     position: relative
     background: rgba(7, 17, 27, .5)
     color: #fff
+    overflow: hidden
     .content-wrapper
       position: relative;
       padding: 24px 12px 18px 24px
@@ -160,6 +181,33 @@
       height: 100%
       z-index: -1
       filter: blur(10px)
+    .detail
+      position: fixed
+      top: 0
+      left: 0
+      z-index: 100
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7, 17, 27, .8)
+      .detail-wrapper
+        min-height: 100%
+        width: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
 
 
